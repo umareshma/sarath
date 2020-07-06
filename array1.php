25929aaa6db5d1f2a20e766ff36d55f78a52134c
umareshma <67307162+umareshma@users.noreply.github.com>
2020-07-06 22:21:06 +0530

    Add files via upload

---
+++ array1.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+ <?php
+ $a = array("orange", "apple","grape","banana");
+ $b = array("cherries","orange","banana");
+ var_dump(array_intersect($a, $b));
+ ?>
+ 
+ </body>
+</html> 
+ 
